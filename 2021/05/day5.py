6f48e5ae24feba1ea56b23203fc68d96e701d324
Tonny Nguyen <tnguyen@ilionx.com>
2021-12-06 17:36:14 +0100

    add wip day 5

---
+++ 2021/05/day5.py
@@ -0,0 +1,14 @@
+with open('example.txt') as f:
+    lines = f.read().splitlines()
+    
+coords = {
+    'x': None,
+    'y': None,
+}
+
+coords_list = []
+
+for line_index, line in enumerate(lines):
+    for index, coords in enumerate(line.split(' -> ')):
+        coords_list.append(coords)
+        print(index)
